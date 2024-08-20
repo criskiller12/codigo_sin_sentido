99efd2d8d4417c45356eea7af63e0f5d3ac8bcff
criskiller12 <145727225+criskiller12@users.noreply.github.com>
2024-08-20 10:39:41 -0500

    Add files via upload

---
+++ traveluserp.sql
@@ -0,0 +1,51 @@
+-- --------------------------------------------------------
+-- Host:                         127.0.0.1
+-- Versión del servidor:         8.0.30 - MySQL Community Server - GPL
+-- SO del servidor:              Win64
+-- HeidiSQL Versión:             12.1.0.6537
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+-- Volcando datos para la tabla transporte.driver: ~0 rows (aproximadamente)
+DELETE FROM `driver`;
+
+-- Volcando datos para la tabla transporte.insurence: ~0 rows (aproximadamente)
+DELETE FROM `insurence`;
+
+-- Volcando datos para la tabla transporte.patient: ~0 rows (aproximadamente)
+DELETE FROM `patient`;
+
+-- Volcando datos para la tabla transporte.plans: ~0 rows (aproximadamente)
+DELETE FROM `plans`;
+
+-- Volcando datos para la tabla transporte.provider: ~0 rows (aproximadamente)
+DELETE FROM `provider`;
+
+-- Volcando datos para la tabla transporte.travelschedule: ~0 rows (aproximadamente)
+DELETE FROM `travelschedule`;
+
+-- Volcando datos para la tabla transporte.travelstatus: ~0 rows (aproximadamente)
+DELETE FROM `travelstatus`;
+
+-- Volcando datos para la tabla transporte.typetravel: ~0 rows (aproximadamente)
+DELETE FROM `typetravel`;
+
+-- Volcando datos para la tabla transporte.typeuser: ~0 rows (aproximadamente)
+DELETE FROM `typeuser`;
+
+-- Volcando datos para la tabla transporte.users: ~0 rows (aproximadamente)
+DELETE FROM `users`;
+
+/*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
